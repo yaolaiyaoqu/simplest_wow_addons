df0c8ddfdae22f225ff6afa13382ab9b9a503d49
wangyao <yaolaiyaoqu55@gmail.com>
2025-10-28 21:43:54 +0800

    Refactor item link check to use string.find
    Updated string matching method for item link.

--- AutoSellTrash/AutoSellTrash.lua
+++ AutoSellTrash/AutoSellTrash.lua
@@ -3,9 +3,10 @@ f:RegisterEvent("MERCHANT_SHOW")
 f:SetScript("OnEvent", function()
     for bag = 0, 4 do
         for slot = 1, GetContainerNumSlots(bag) do
-            if (GetContainerItemLink(bag,slot) or ""):find("ff9d9d9d") then
+            if string.find((GetContainerItemLink(bag,slot) or ""), "ff9d9d9d") then
                 UseContainerItem(bag, slot)
             end
         end
     end
 end)
+
